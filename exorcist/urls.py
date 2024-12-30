df8f2188ce7983bb0ee3faa0488b247034e5cc91
Didilya <didiyarova13@gmail.com>
2024-12-30 15:15:32 +0300

    revert urlpatterns for static

--- exorcist/urls.py
+++ exorcist/urls.py
@@ -17,13 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path
-from django.views.static import serve
-from . import settings
+# from django.urls import re_path
+# from django.views.static import serve
+# from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("", include("users.urls")),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]

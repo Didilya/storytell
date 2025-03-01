f61dca5c1493b4b85885543b77c5c818a3ccea26
Didilya <didiyarova13@gmail.com>
2025-03-01 23:27:10 +0300

    get_first_llm_response utils exception added

--- core/utils/deep_seek.py
+++ core/utils/deep_seek.py
@@ -1,18 +1,22 @@
 # install OpenAI SDK first: `pip3 install openai`
-from core.settins core_settings
-from openai import OpenAI
+from core.settings import core_settings
+from openai import OpenAI, APIStatusError
 
 client = OpenAI(api_key=core_settings.LLM_NODEL_API_KEY, base_url="https://api.deepseek.com")
 
 def get_first_llm_response(question: str) -> str| None:
-    if isinstance(question, str):
-        response = client.chat.completions.create(
-            model="deepseek-chat",
-            messages=[
-                {"role": "system", "content": question},
-            ],
-            stream=False
-        )
+    try:
+        if isinstance(question, str):
+            response = client.chat.completions.create(
+                model="deepseek-chat",
+                messages=[
+                    {"role": "system", "content": question},
+                ],
+                stream=False
+            )
 
-        return response.choices[0].message.content
-    return None
+            return response.choices[0].message.content
+    except APIStatusError as e:
+        print(f" openai error {e}")
+    finally:
+        return None

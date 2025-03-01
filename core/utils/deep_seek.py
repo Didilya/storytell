9ea7093ae5005d489a3e5973e7fcba3752b64509
Didilya <didiyarova13@gmail.com>
2025-03-01 23:21:47 +0300

    get_first_llm_response utils

--- core/utils/deep_seek.py
+++ core/utils/deep_seek.py
@@ -1,16 +1,18 @@
-# Please install OpenAI SDK first: `pip3 install openai`
+# install OpenAI SDK first: `pip3 install openai`
 from core.settins core_settings
 from openai import OpenAI
 
 client = OpenAI(api_key=core_settings.LLM_NODEL_API_KEY, base_url="https://api.deepseek.com")
 
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
-    ],
-    stream=False
-)
+def get_first_llm_response(question: str) -> str| None:
+    if isinstance(question, str):
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {"role": "system", "content": question},
+            ],
+            stream=False
+        )
 
-print(response.choices[0].message.content)
+        return response.choices[0].message.content
+    return None
